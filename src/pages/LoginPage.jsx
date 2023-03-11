a6d64afc1afe3e55c6bab7365aa4345d378dedd7
igornayan <igornayan@hotmail.com>
2023-03-11 15:41:21 -0300

    Página de Login finalizado

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,12 +8,19 @@ function LoginPage()
 
             <div className="h-14 flex items-center">
                 <Link className="underline text-blue-700" to="/">
-                <img className="h-10 cursor-pointer" src="/moura logo.png" alt="" />
+                <img className="h-10 cursor-pointer ml-5" src="/moura logo.png" alt="" />
                 </Link>
             </div>
 
-            <div className="grow bg-gray-100 flex flex-col items-center">
-                <p className="text-3xl fonte mt-36">Entre com sua conta</p>
+            <div className="grow bg-gray-100 flex flex-col items-center justify-center">
+                <div className="relative w-1/4 flex flex-col items-center justify-center">
+                    <img className="w-1/3 mb-5" src="/Moura logo 2.png" alt="" />
+                    <p className="fonte text-4xl mb-28 font-semibold">Entre com sua conta</p>
+                    <input className="h-12 w-full mb-1.5 rounded-md  p-2.5" type="text" placeholder="Email"></input>
+                    <input className="h-12 w-full rounded-md p-2.5" type="password" placeholder="Senha"></input>
+                    <button className="w-full h-11 mt-14 bg-blue-700 rounded-md text-white">Entrar</button>
+                    <p className="absolute right-0 bottom-16 text-blue-700">Esqueceu a senha ?</p>
+                </div>
             </div>
         </div>
     )
